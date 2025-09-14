fdf4717a6b32f1c693d3f6b7dc77c4b03e7b89f7
Difa Nugraha <123245678+DifaaNug@users.noreply.github.com>
2025-09-14 13:56:21 +0700

    Remove SiadilHeader from dashboard page
    The SiadilHeader component has been removed from the DashboardPage, simplifying the layout and reducing unnecessary wrapper elements.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,9 @@
-import SiadilHeader from "@/components/SiadilHeader";
-
 export default function DashboardPage() {
   return (
-    <div className="min-h-screen">
-      <SiadilHeader />
+    <>
       <div className="py-10 px-10 font-bold text-gray-800 text-lg">
         Ini Halaman Dashboard
       </div>
-    </div>
+    </>
   );
 }
