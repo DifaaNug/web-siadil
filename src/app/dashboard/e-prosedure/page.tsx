cbd6b4950a1c6de6dd09eba063c12b056e26d97b
Difa Nugraha <123245678+DifaaNug@users.noreply.github.com>
2025-09-15 14:53:17 +0700

    Add E-Prosedure dashboard page
    Introduces a new page component for the E-Prosedure section in the dashboard, displaying a simple header message.

---
+++ src/app/dashboard/e-prosedure/page.tsx
@@ -0,0 +1,9 @@
+export default function EprosedurePage() {
+  return (
+    <>
+      <div className="py-10 px-10 font-bold text-gray-800 text-lg">
+        Ini Halaman E-Prosedure
+      </div>
+    </>
+  );
+}
