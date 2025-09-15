01ebf9bf056789bd262b122279b31b9f9d0f22b7
Difa Nugraha <123245678+DifaaNug@users.noreply.github.com>
2025-09-15 14:53:34 +0700

    Add Shortlink dashboard page component
    Introduces ShortlinkPage component for the dashboard, displaying a placeholder message. This sets up the structure for future shortlink-related features.

---
+++ src/app/dashboard/shortlink/page.tsx
@@ -0,0 +1,9 @@
+export default function ShortlinkPage() {
+  return (
+    <>
+      <div className="py-10 px-10 font-bold text-gray-800 text-lg">
+        Ini Halaman Shortlink
+      </div>
+    </>
+  );
+}
