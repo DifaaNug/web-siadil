9094490e1e485e9656f00f9ce7dd82a2cf6c615f
Difa Nugraha <123245678+DifaaNug@users.noreply.github.com>
2025-09-15 14:53:30 +0700

    Add LibraryPage component
    Introduces a new LibraryPage component displaying a styled message. This sets up the basic structure for the library dashboard page.

---
+++ src/app/dashboard/library/page.tsx
@@ -0,0 +1,9 @@
+export default function LibraryPage() {
+  return (
+    <>
+      <div className="py-10 px-10 font-bold text-gray-800 text-lg">
+        Ini Halaman Library
+      </div>
+    </>
+  );
+}
